279ec6258559e4e32b4183bc888a4c25c233b8b3
dancrew32 <dancrew32@gmail.com>
2013-04-02 17:13:20 -0700

    common.index.php example for new installers

---
+++ view/common.index.php
@@ -0,0 +1,17 @@
+<div id="main">
+	<h1>
+		My name is Marcel
+	</h1>
+	<p>
+		And I'm partially a shell as you can see on my body.
+	</p>
+	<p>
+		But I have shoes and a face. And I like myself.
+	</p>
+</div>
+<div id="sidebar-a">
+
+</div>
+<div id="sidebar-b">
+
+</div>
