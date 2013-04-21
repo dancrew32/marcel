38b51cc160ea6278bb7986c91113449decc2d781
dancrew32 <dancrew32@gmail.com>
2013-04-21 00:35:40 -0700

    let script/worker.php take an argument of threads

--- script/worker.php
+++ script/worker.php
@@ -1,26 +1,25 @@
 <?
 require_once(dirname(__FILE__).'/inc.php');
 
-$threads = 10;
+$threads = isset($argv[1]{0}) ? $argv[1] : 10;
 $thread_store = [];
 for ($i = 0; $i < $threads; $i++) {
 	$pid = pcntl_fork();
+	$thread_store[] = $pid;			
 	if ($pid === 0) {
 		while(true) {
 			$ws = Worker::find('all', [
 				'conditions' => ['active = 0'],
-				'limit' => 10,
+				'limit' => 1,
 			]);
 			foreach ($ws as $w) {
 				$w->run($pid);	
 			}
 			sleep(1);
 		}
-	} else {
-		$thread_store[] = $pid;			
-	}
+	} else
+		$thread_store[] = $pid;
 }
 
 foreach ($thread_store as $pid)
 	pcntl_waitpid($pid, $status);
-echo "Done\n";
