e60f469e090e9f5b4a1794b7b0e30f8b9b2ae2c7
dancrew32 <dancrew32@gmail.com>
2013-05-26 00:57:31 -0700

    make space for usps api user/pass

--- config/api.php
+++ config/api.php
@@ -2,16 +2,26 @@
 api::$keys = [
 	'stripe' => [ # https://manage.stripe.com/account/apikeys
 		'DEV' => [
-			'secret' => 'sk_test...',
-			'public' => 'pk_test...',
+			'secret' => 'sk_test_',
+			'public' => 'pk_test_',
 		],
 		'LIVE' => [
-			'secret' => 'sk_live...',
-			'public' => 'pk_live...',
+			'secret' => 'sk_live_',
+			'public' => 'pk_live_',
 		],
 	],
 	'google_maps' => [ # https://code.google.com/apis/console/b/0/
 		'DEV'  => '',
 		'LIVE' => '',
+	],
+	'usps' => [ # https://secure.shippingapis.com/registration/
+		'DEV' => [
+			'username' => '',
+			'password' => '',
+		],
+		'LIVE' => [
+			'username' => '',
+			'password' => '',
+		],
 	],
 ];
