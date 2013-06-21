afb2dc7e7f88d6046d2b879e715588231b585723
www-data <www-data@danmasq.com>
2013-06-20 17:33:20 -0700

    add metas, closes #88

--- view/layout/a.php
+++ view/layout/a.php
@@ -6,6 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
 <meta name="viewport" content="width=device-width">
+<meta name="referrer" content="always">
 <?= $css ?>
 <!--[if lt IE 9]><script src="/js/html.shiv.js"></script><![endif]-->
 </head>
